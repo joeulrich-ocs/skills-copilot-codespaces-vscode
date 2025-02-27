c4c8cfacc0e315e8c13f9ad069f45f0da4bfc5cc
joeulrich-ocs <joe.ulrich@drivetime.com>
2025-02-27 15:23:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = {
+    name: 'Joe Ulrich',
+    age: 43,
+    skills: ['Genesys Cloud Architect', 'Python', 'Being Awesome'],
+    address: {
+      city: 'Wisconsin',
+      country: 'USA'
+    }
+  };
+  return member;
+}
